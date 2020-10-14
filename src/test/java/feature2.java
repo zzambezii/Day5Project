c61d6ff68ccc89dbd518adef6974fdeae0bc491e
zzambezii <zambezide@gmail.com>
2020-10-15 01:43:25 +0200

    feature2 change added more

--- src/test/java/feature2.java
+++ src/test/java/feature2.java
@@ -2,6 +2,9 @@ public class feature2 {
 
     public static void main(String[] args) {
         System.out.println("working on f2");
+        System.out.println("working 2");
+        System.out.println("working agaihng");
+
 
     }
 }
