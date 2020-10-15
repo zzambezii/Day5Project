7fe089d2135e5df0d08a051d5d2747453ebd1c7e
zzambezii <zambezide@gmail.com>
2020-10-15 02:10:40 +0200

    updated the way say hello2

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("hello love really awesome hello");
+        System.out.println("hello love really awesome helloooo");
     }
 }
