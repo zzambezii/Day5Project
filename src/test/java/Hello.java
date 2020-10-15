8ecfa451074306d8f1c77027b93d74ca8df62636
zzambezii <zambezide@gmail.com>
2020-10-15 02:50:30 +0200

    adding some lines not push yet

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello love really awesome helloooo FROM GITUP"); //gitup comes AGAIN
         //some more changes here form gitup
-
+        //adding some awsomce chnages
 
     }
 }
