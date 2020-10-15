6070a7075462521ba743cafdd1b5c8826b7d5cb3
zzambezii <zambezide@gmail.com>
2020-10-15 02:28:17 +0200

    added in same line a commit

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("hello love really awesome helloooo");
+        System.out.println("hello love really awesome helloooo");//add one mor line in your local and commit
+
+
     }
 }
